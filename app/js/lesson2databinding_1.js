244357eb0b7d6c0aa281161ecf6599ce1e17c098
Cotardo <Silvia.Cotardo.consultant@nielsen.com>
2016-11-23 19:25:07 -0600

    Practica 1 Lesson 2

---
+++ app/js/lesson2databinding_1.js
@@ -0,0 +1,4 @@
+var lesson2App_var = angular.module("lesson2App_html", []);
+var indexController_ = lesson2App_var.controller("indexController", function ($scope) {
+$scope.message = "Hello World"
+});
